14805fa2c6a627e700c0881e121a9e68c39efbfe
jackymomomo <jacklloydrobinson@gmail.com>
2023-01-19 09:48:18 -0800

    added minor changes to play.js, and added key functionailty to move my snake, also updated the setinput function to take in a connection argument

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 // setup interface to handle user input from stdin
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -16,8 +18,25 @@ const handleUserInput = (key) => {
     if (key === '\u0003') {
       process.exit();
     }
+    if (key === 'w') {
+      connection.write("Move: up")
+    }
+    if (key === 'a') {
+      connection.write("Move: left")
+    }
+    if (key === 's') {
+      connection.write("Move: down")
+    }
+    if (key === 'd') {
+      connection.write("Move: right")
+    }
   })
 }
+
+// const MOVE_UP_KEY = 'w';
+// const MOVE_LEFT_KEY = 'a';
+// const MOVE_DOWN_KEY = 's';
+// const MOVE_RIGHT_KEY = 'd';
 
 handleUserInput();
 setupInput();
