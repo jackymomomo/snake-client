53c278eca8853ef3f2e7ef387d852864a37a53ec
jackymomomo <jacklloydrobinson@gmail.com>
2023-01-19 08:51:03 -0800

    plays a established connection to the server message when a client connects

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',  // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log('connecting Estalbished')
+
+    conn.write()
+  })
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
