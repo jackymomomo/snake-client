8dc4ca0ffbbf2e88f7550221415f1a4049862b82
jackymomomo <jacklloydrobinson@gmail.com>
2023-01-19 15:16:07 -0800

    added more constants for move keys

--- client.js
+++ client.js
@@ -3,8 +3,8 @@ const { IP, PORT } = require('./constants')
 
 const connect = function () {
   const conn = net.createConnection({
-    IP, 
-    PORT
+    host: 'localhost',
+    port: 50541, // wont let me use the IP and or PORT constants :|
   });
 
   // interpret incoming data as text
--- constants.js
+++ constants.js
@@ -1,6 +1,24 @@
-const IP = 'localhost'
 const PORT = 50541;
+const IP = 'localhost';
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+const MESSAGES = {
+  '1': 'Say: SsSssSs',
+  '2': 'Say: GET OUT MY HOUSE!',
+  '3': 'Say: nom nom nom',
+  '4': 'Say: ❤️❤️❤️',
+  '5': 'Say: HAHA gotcha bitch'
+};
 
-module.exports = { IP, PORT };
 
-
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  MOVE_DOWN_KEY,
+  MESSAGES
+};
