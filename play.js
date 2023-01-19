84e6e9f83d69cb027461fa5b80d7e3c18c1945e4
jackymomomo <jacklloydrobinson@gmail.com>
2023-01-19 08:56:15 -0800

    able to connect and see your snake appear on the board with name

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const net = require("net");
-
+const { connect } = require('./client')
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',  // IP address here,
-    port: 50541 // PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('connect', () => {
-    console.log('connecting Estalbished')
-
-    conn.write()
-  })
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
